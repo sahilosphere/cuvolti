cf3757e96ac6880d3bf7d1f80c4b4f973d3c77d3
sahilosphere <sahilosphere@gmail.com>
2026-09-25 18:22:10 +0530

    Add files via upload

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/admin": "admin" });
+  eleventyConfig.ignores.add("src/admin/**");
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return new Date(dateObj).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
+
+  eleventyConfig.addCollection("articles", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("src/articles/*.md").sort((a, b) => {
+      return b.date - a.date;
+    });
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
+};
